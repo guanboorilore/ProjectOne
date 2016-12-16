b872beecb0f1a9576e921038f22386b3a966c732
guanboorilore <77174381@qq.com>
2016-12-16 14:37:27 +0800

    Main

---
+++ GitProject/src/com/orilore/gb/Test/Main.java
@@ -0,0 +1,14 @@
+package com.orilore.gb.Test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("欢迎大家来到git世界");
+		
+		
+	}
+
+}
